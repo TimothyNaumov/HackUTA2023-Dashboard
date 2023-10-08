fc7f02efb97c5aef5678cd4b394f959eeb4a9811
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2023-10-08 01:31:25 -0500

    functional sound device selection

--- backend/AudibleResponse.py
+++ backend/AudibleResponse.py
@@ -1,8 +1,9 @@
 import os
 import requests
-from pydub.playback import play
-from playsound import playsound
 from dotenv import load_dotenv
+import sounddevice as sd
+import soundfile as sf
+import time
 
 load_dotenv()  # take environment variables from .env.
 
@@ -19,18 +20,27 @@ headers = {
     "xi-api-key": xi_api_key,
 }
 
-data = {
-    "text": "Your tires are estimated to last until 2029 or 50,000 miles. Currently, your vehicle has 60,000 miles. Your tires should still be in good condition. Regular inspections are recommended to ensure safety.",
-    "model_id": "eleven_monolingual_v1",
-    "voice_settings": {"stability": 0.5, "similarity_boost": 0.5},
-}
+sound_device = 6
 
-response = requests.post(url, json=data, headers=headers, stream=True)
 
-with open("output.mp3", "wb") as f:
-    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-        if chunk:
-            f.write(chunk)
+def speak(message):
+    data = {
+        "text": message,
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {"stability": 0.5, "similarity_boost": 0.5},
+    }
 
-print("We have a response!")
-playsound("output.mp3")
+    response = requests.post(url, json=data, headers=headers, stream=True)
+
+    with open("output.mp3", "wb") as f:
+        start_time = time.time()  # record the start time
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+
+    elapsed_time = time.time() - start_time  # calculate the elapsed time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")  # print the elapsed time
+
+    data, fs = sf.read("output.mp3")
+    sd.play(data, fs, device=sound_device)
+    sd.wait()
